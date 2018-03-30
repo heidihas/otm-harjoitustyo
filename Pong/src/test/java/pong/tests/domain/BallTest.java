511a9b1160510905e41e6f519a34aa78bec8539a
Heidi <heidi.hassinen@helsinki.fi>
2018-03-30 17:54:06 +0300

    testi

---
+++ Pong/src/test/java/pong/tests/domain/BallTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong.tests.domain;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pong.domain.Ball;
+
+/**
+ *
+ * @author Heidi
+ */
+public class BallTest {
+    
+    Ball ball;
+    
+    @Before
+    public void setUp() {
+        ball = new Ball(0, 0, 10);
+    }
+    
+    @Test
+    public void luotuKorttiOlemassa() {
+        assertTrue(ball!=null);      
+    }
+    
+}
